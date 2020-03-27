65dd51f9dcd6f31f2e56b50024322d09e313bf86
ousttrue <ousttrue@gmail.com>
2020-03-27 22:11:48 +0900

    init

---
+++ FunnelPipe/main.cpp
@@ -0,0 +1,53 @@
+// #include "Application.h"
+// #include "save_windowplacement.h"
+// #include "frame_metrics.h"
+#include <Win32Window.h>
+#include <filesystem>
+#include <iostream>
+
+const auto CLASS_NAME = L"FunnelPipeClass";
+const auto WINDOW_NAME = L"FunnelPipe";
+
+int main(int argc, char **argv)
+{
+    screenstate::Win32Window window(CLASS_NAME);
+    auto hwnd = window.Create(WINDOW_NAME);
+    if (!hwnd)
+    {
+        std::cerr << "fail to window.Create";
+        return 1;
+    }
+    window.Show();
+
+    // Application app(argc, argv);
+
+    // auto windowconf = std::filesystem::current_path().append("FunnelPipe.window.json").u16string();
+    // windowplacement::Restore(hwnd, SW_SHOW, (const wchar_t *)windowconf.c_str());
+    // window.OnDestroy = [hwnd, conf = windowconf]() {
+    //     windowplacement::Save(hwnd, (const wchar_t *)conf.c_str());
+    // };
+
+    {
+        {
+            screenstate::ScreenState state;
+            while (true)
+            {
+                // frame_metrics::new_frame();
+                // frame_metrics::scoped s("frame");
+                // {
+                //     frame_metrics::scoped ss("window");
+                    if (!window.TryGetInput(&state))
+                    {
+                        break;
+                    }
+                // }
+                // {
+                //     frame_metrics::scoped ss("app");
+                //     app.OnFrame(hwnd, state);
+                // }
+            }
+        }
+    }
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # FunnelPipe
 D3D12 experiment
+
+* glTF viewer
+
+```
+scene+
+     |
+     v
+
+\       /
+ \     /
+  \   /
+   | |
+   v v
+ drawlist
+   v v
+d3d12 pipeline
+```
